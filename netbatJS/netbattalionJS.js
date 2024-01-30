05f36189c35cfa8a9b616b82ccb0ba67f5fc0d2f
netbattalion.de <98064002+DaBusch@users.noreply.github.com>
2024-01-30 02:53:58 +0100

    Add files via upload

--- netbatJS/netbattalionJS.js
+++ netbatJS/netbattalionJS.js
@@ -1,25 +1,22 @@
-!(() => {
-    let $ = i => i.dataset,
-        r = {},
-        s = {},
-        o = document.querySelectorAll("[data-bind]"),
-        f = (v) => {
-            for (i in d = data[v]) this[i] = d[i]
-        }
-    data.init()
-    f('properties')
-    f('methods')
-    for (i of o) {
-        let t = $(i).bind.split(",")
-        r[t[0]] = t[1] || i.innerHTML
-        y = t[0]
-        !(s[y]) && (q = s[y] = [])
-        q.push([i, eval(`()=>{${i.dataset.set}}`)])
+!( () => {
+    let i, q, r = {}, s = {}, e = data,
+        o = document .querySelectorAll( '[data-bind]' )
+    e .init()
+    for ( i in e ) i != 'init' && ( x => {
+        for ( i in d = e[ i ] ) self[ i ] = d[ i ]
+    } ) ()
+    for ( i of o ) {
+        let u = i .dataset,
+        t = u .bind .split( "," ),
+        y = t[ 0 ]
+        r[ y ] = t[ 1 ] || i .innerHTML
+        !s[ y ] && ( q = s[y] = [] )
+        q .push( [ i, Function(u.set) ] )
     }
-    for (z in ref = new Proxy(r, {
-        set: (i, t, e) => {
-            i[t] = e
-            for (f of s[t]) f[1](self = f[0])
+    for ( i in ref = new Proxy( r, {
+        set: ( k, t, e ) => {
+            k[ t ] = e
+            for ( i of s[ t ] ) i[ 1 ] ( self = i[0] )
         }
-    })) ref[z] = r[z]
-})()
+    } ) ) ref[ i ] = r[ i ]
+} ) ()
