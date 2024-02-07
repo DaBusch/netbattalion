e5e2595e4e85014d9094918d7a029be37272151a
netbattalion.de <98064002+DaBusch@users.noreply.github.com>
2024-02-07 07:24:25 +0100

    Update netbattalionJS.js

--- netbatJS/netbattalionJS.js
+++ netbatJS/netbattalionJS.js
@@ -1,22 +1,20 @@
-!( () => {
-    let i, q, r = {}, s = {}, e = data,
-        o = document .querySelectorAll( '[data-bind]' )
-    e .init()
-    for ( i in e ) i != 'init' && ( x => {
-        for ( i in d = e[ i ] ) self[ i ] = d[ i ]
-    } ) ()
-    for ( i of o ) {
+! ( ( ) => {
+    let q, r = {}, s = {},
+    o = document .querySelectorAll( "[data-bind]" )
+    for( i of o ) {
         let u = i .dataset,
-        t = u .bind .split( "," ),
-        y = t[ 0 ]
-        r[ y ] = t[ 1 ] || i .innerHTML
-        !s[ y ] && ( q = s[y] = [] )
-        q .push( [ i, Function(u.set) ] )
+        t = u. bind .trim() .split(","),
+        y = t[ 0 ],
+        v = t[ 1 ]
+        v ? r[ y ] = eval( v ) ?? i.innerHTML : null
+        s[ y ] ?? ( q = s[ y ] = [] ),
+        q .push( [ i, Function( u .set ) ] )
     }
-    for ( i in ref = new Proxy( r, {
-        set: ( k, t, e ) => {
-            k[ t ] = e
-            for ( i of s[ t ] ) i[ 1 ] ( self = i[0] )
+    for ( i in ref = new Proxy(
+        r, {
+            set: ( k, t, e ) => {
+                for ( i of ( k[ t ] = e, s[ t ] ) ) i[ 1 ] ( me = i[ 0 ] ) 
+            }
         }
-    } ) ) ref[ i ] = r[ i ]
-} ) ()
+    ) ) ref[i]=r[i]
+} )()
