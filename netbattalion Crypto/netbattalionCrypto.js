7e75bdfbf78b8161fe3cb4cd00236fa01f6cf0de
netbattalion.de <98064002+DaBusch@users.noreply.github.com>
2024-02-12 03:49:23 +0100

    Add files via upload

--- netbattalion Crypto/netbattalionCrypto.js
+++ netbattalion Crypto/netbattalionCrypto.js
@@ -3,51 +3,47 @@ enc = i => new TextEncoder() .encode(i),
 dec = i => new TextDecoder() .decode(i),
 i = 192,
 cs = crypto .subtle,
-siv = e => Array .from( crypto .getRandomValues( new Uint8Array( e ) ) ) .toString(),
-
-saltIvString = siv(16),                            //  SaltIVArrayString Returns number-CSV
-iv = siv(16),
-b = (salt) => new Uint8Array( salt.split(',') ),   // SaltIvBuffer Returns UInt8Array
+siv = e => Array .from( crypto .getRandomValues( new Uint8Array( e ) ) ) .toString(),//  SaltIVArrayString Returns number-CSV
+b = (salt) => new Uint8Array( salt.split(',') ),                                     //  SaltIvBuffer Returns UInt8Array
 
 // ----------------------------------- GENERATE KEY -----------------
 
 // create the first key
-iK = async (pw) => await cs.importKey(
-    'raw', enc(pw), 'PBKDF2', false, ['deriveKey']
+iK = async ( pw ) => await cs .importKey(
+    'raw', enc( pw ), 'PBKDF2', false, [ 'deriveKey' ]
 ),
 // create the second key
-dK = async (pw, salt) => await cs.deriveKey( 
-    { name: "PBKDF2", salt: b(salt), iterations: 250000, hash: "SHA-256", },
-    await iK(pw),
+dK = async ( p, salt ) => await cs .deriveKey( 
+    { name: "PBKDF2", salt: b(salt), iterations: 250000, hash: "SHA-256" },
+    await iK( p ),
     { name: "AES-GCM", length: 256 },
     true,
-    ['encrypt', 'decrypt']
+    [ 'encrypt', 'decrypt' ]
 ),
 
 // ----------------------------------- ENCRYPT AND DECRYPT MESSAGE --------------
 
-kT = async (key,s) => s ? await dK(key,s) : key,
-mode = { name: "AES-GCM", iv: b( iv ) }
+kT = async ( key, s ) => s ? await dK( key, s ) : key,
 
-enAES = async (key, mess, iv, s) => {
+enAES = async ( key, mess, iv, s ) => {
     let e = [],
-    m = await cs.encrypt(
-        mode,
+    m = await cs .encrypt(
+        { name: "AES-GCM", iv: b( iv ) },
         await kT( key, s ),
         enc( mess )
     )
-    for ( v of new Uint8Array(m)) {        
+    for ( v of new Uint8Array( m )) {        
         e .push( String .fromCharCode( v + i ) ) }
         e = e .join( '' )
     return e
 },
 
-deAES = async (key, encrypted, v, s) => {
+deAES = async ( k, enc, v, s ) => {
     let c = [], t
-    for( t of encrypted ) c .push( t .charCodeAt( 0 ) - i );
-    let d = await cs.decrypt(
-        mode,
-        await kT( key, s ),
+    for( t of enc ) c .push( t .charCodeAt( 0 ) - i );
+    let d = await cs .decrypt(
+        { name: "AES-GCM", iv: b( v ) },
+        await kT( k, s ),
         new Uint8Array( c )
     )
     return dec( d )
@@ -55,68 +51,66 @@ deAES = async (key, encrypted, v, s) => {
 
 // ------------------------------- DIFFIE hELLMAN KEY EXCHANGE ------
 
-genECDHKeyPair = async (use) => {
-    return await cs.generateKey(
+genECDHKeyPair = async () => {
+    return await cs .generateKey(
     {
         name: "ECDH",
         namedCurve: "P-521"
     },
     true,
-    use
+    [ 'deriveKey' ]
     )
   },
 
-deriveSecretKey = async (privateKey, publicKey) => {
-    return await cs.deriveKey(
+deriveSecretKey = async ( prK, puK ) => {
+    return await cs .deriveKey(
         {
             name: "ECDH",
-            public: publicKey
+            public: puK
         },
-        privateKey,
+        prK,
         {
             name: "AES-GCM",
             length: 256
         },
         false,
-        ["encrypt", "decrypt"]
+        [ "encrypt", "decrypt" ]
     )
 },
 
 // ------------------------------- SIGNATE AND VERIFY MESSAGES ------
 
 genECDSAKeyPair = async () => {    
-    return await cs.generateKey(
+    return await cs .generateKey(
         {
             name: "ECDSA",
             namedCurve: "P-521",
         },
         true,                  //           
-        ["sign", "verify"]
+        [ "sign", "verify" ]
     )
 },
 
-signMessage = async (mess, privateKey) => {
-    let m = enc(mess)
-    return await cs.sign(
+signMessage = async ( m, prK ) => {
+    return await cs .sign(
     {
         name: "ECDSA",
-        hash: {name: "SHA-384"},
+        hash: { name: "SHA-384" },
     },
-    privateKey,
-    m
+    prK,
+    enc( m )
     )
  },
 
- verifyMessage = async (mess, publicKey, signature) => {
-    let e = enc(mess)
-    return await cs.verify(
+ verifyMessage = async ( m, puK, s ) => {
+    return await cs .verify(
       {
         name: "ECDSA",
-        hash: {name: "SHA-384"},
+        hash: { name: "SHA-384" },
       },
-      publicKey,
-      signature,
-      e
+      puK,
+      s,
+      enc( m )
     );
  }
 
