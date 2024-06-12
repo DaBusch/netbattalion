5ab27de86c4a03196718256c9884ae6d0345800b
netbattalion.de <98064002+DaBusch@users.noreply.github.com>
2024-06-13 00:15:33 +0200

    Add files via upload

--- netbatJS/netbattalionJS.js
+++ netbatJS/netbattalionJS.js
@@ -1,26 +1,23 @@
 ; ( ( ) => {
-    let r = {}, s = {},
-    o = document .querySelectorAll( "[data-bind]" )
-    for( i of o ) {
-        let u = i .dataset,
-
+    let r = {}, f = {}
+    for( i of document .querySelectorAll( "[data-bind]" ) ) {
+        let d = i .dataset,
 // ---- Variables ----------------------------------------
-        t = u. bind .trim() .split(","),
-        y = t[ 0 ],
-        v = t[ 1 ]
-        v && (r[ y ] = eval( v ) ?? i.innerHTML)
+        b = d .bind .split("="),
+        y = b[ 0 ] .trim()
+        b[ 1 ] && ( r[ y ] = eval( b[ 1 ] ) )
 
 // ---- Functions ----------------------------------------
-        s[ y ] ?? ( s[ y ] = [] ),
-        s[ y ] .push( [ i, Function( u .set ) ] )
+        w = f[ y ] ?? ( f[ y ] = [] ),
+        w .push( [ i, Function( d .set ) ] )
     }
 
 // ---- Observe ------------------------------------------
     ref = new Proxy(
         r, {
-            set: ( k, t, e ) => {
-                k[ t ] = e
-                for ( i of s[ t ] ) i[ 1 ] ( me = i[ 0 ] ) 
+            set: ( r, p, v ) => {
+                r[ p ] = v
+                for ( i of f[ p ] ) i[ 1 ] ( me = i[ 0 ] ) 
             }
         }
     )
